961ff98bb2ef06374769b575c09a2af09f36789a
Leonardo <casanoval836@gmail.com>
2025-12-05 19:20:10 -0300

    Update require path for auth in dashboard.php

--- trabalho-final/public/admin/dashboard.php
+++ trabalho-final/public/admin/dashboard.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/inc/auth.php';
+require_once __DIR__ . '/inicio/auth.php';
 require_once __DIR__ . '/../../src/db.php';
 require_once __DIR__ . '/../../src/perguntas.php';
 
--- trabalho-final/public/admin/perguntas.php
+++ trabalho-final/public/admin/perguntas.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/inc/auth.php';
+require_once __DIR__ . '/inicio/auth.php';
 require_once __DIR__ . '/../../src/perguntas.php';
 
 $acao = $_GET['acao'] ?? '';
